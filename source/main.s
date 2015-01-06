a86b559a90388db0e94b97baee399e9819f73a69
Skylar Schipper <ss@schipp.co>
2015-01-05 16:19:18 -0800

    Power on 'ACT' LED

--- source/main.s
+++ source/main.s
@@ -12,3 +12,14 @@ _start:
 
   // Enable GPIO
   mov r1,#1
+  lsl r1,#18
+  str r1,[r0,#4]
+
+  // Turn off the pin to turn on the LED
+  mov r1,#1
+  lsl r1,#16
+  str r1,[r0,#40]
+
+  // Loop forever
+  loop$:
+  b loop$
