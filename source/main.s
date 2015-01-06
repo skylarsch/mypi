0f8c9bad0be963b5c20cd3e0703769737a04c8ff
Skylar Schipper <ss@schipp.co>
2015-01-05 17:07:16 -0800

    Flash LED

--- source/main.s
+++ source/main.s
@@ -14,12 +14,26 @@ _start:
   mov r1,#1
   lsl r1,#18
   str r1,[r0,#4]
-
-  // Turn off the pin to turn on the LED
+  
   mov r1,#1
   lsl r1,#16
-  str r1,[r0,#40]
 
-  // Loop forever
   loop$:
+
+    str r1,[r0,#40]
+
+    mov r2,#0x3F0000
+    wait1$:
+      sub r2,#1
+      cmp r2,#0
+      bne wait1$
+
+    str r1,[r0,#28]
+
+    mov r2,#0x3F0000
+    wait2$:
+      sub r2,#1
+      cmp r2,#0
+      bne wait2$
+
   b loop$
